a282c0cc9c9d906458038a2bb9bafce0caaf2f6a
jgiere <johannes@jgiere.de>
2017-11-23 20:43:05 +0100

    Endpoint refreshed

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ PASSWORD = "";
 -- 15 * 60 seconds sleep (15 minutes)
 SLEEP_TIME = 15 * 60 * 1000000;
 
-SERVER = "http://127.0.0.1";
+SERVER = "http://127.0.0.1/HomeWeatherStationAPI/api";
 MEASURE_SCRIPT = "measure.php";
 
 SERVER_ENDPOINT = string.format("%s/%s", SERVER, MEASURE_SCRIPT);
