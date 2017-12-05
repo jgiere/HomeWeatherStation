f53e133379c24871e85b5438567e25fd91152742
Johannes Giere <johannes@jgiere.de>
2017-12-05 08:43:53 +0100

    BugFix for the TSL2561 reading

--- application.lua
+++ application.lua
@@ -12,8 +12,6 @@ function readTempAndHumid(chipId, battery, brightness)
         chipId,
         brightness
     );
-
-    print(SERVER_ENDPOINT);
     
     http.post( SERVER_ENDPOINT, 
         "Content-Type: application/x-www-form-urlencoded\r\n",
@@ -22,7 +20,7 @@ function readTempAndHumid(chipId, battery, brightness)
             print(data);
 
             print("Measurement successfull ... going to sleep");
-            --node.dsleep(SLEEP_TIME);
+            node.dsleep(SLEEP_TIME);
         end
     )
 end
@@ -32,10 +30,13 @@ local chipId = node.chipid();
 
 -- Read battery life
 -- BUG: This does not work.
-local battery = adc.readvdd33();
+local battery = adc.read(0)*4/978;
 
 -- Read brightness
-local brightnessStatus = tsl2561.init(2, 1, tsl2561.ADDRESS_FLOAT, tsl2561.PACKAGE_T_FN_CL)
+local brightnessStatus = tsl2561.init(2, 1, tsl2561.ADDRESS_FLOAT, tsl2561.PACKAGE_T_FN_CL);
+if status == tsl2561.TSL2561_OK then
+    status = tsl2561.settiming(tsl2561.INTEGRATIONTIME_402MS, tsl2561.GAIN_1X);
+end
 if brightnessStatus == tsl2561.TSL2561_OK then
     local lux = tsl2561.getlux();
     readTempAndHumid(chipId, battery, lux);
