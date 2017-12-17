7b9fcebf61116018f1a5d90b90562b8142e75c87
Johannes Giere <johannes@jgiere.de>
2017-12-17 18:08:35 +0100

    Added DS18B20 support

--- application.lua
+++ application.lua
@@ -1,15 +1,25 @@
-function readTempAndHumid(chipId, battery, brightness)
 
-    status, temp, humi, temp_dec, humi_dec = dht.read(PIN_DHT);
+dhtTemp = nil;
+dhtTempDec = nil;
+humidity = nil;
+humidityDec = nil;
+battery = nil;
+lux = nil;
+ds18b20Temp = nil;
+-- Get chipId.
+chipId = node.chipid();
     
-    jsonBody = string.format(' { "temperature": %s, "humidity": %s.%s, "battery": %s, "device": %s, "brightness": %s }' ,
-        temp,
-        math.floor(humi),
-        humi_dec,
+function sendData()
+    jsonBody = string.format(' { "temperature": %s, "humidity": %s.%s, "battery": %s, "device": %s, "brightness": %s, "temperature_2": %s }' ,
+        dhtTemp,
+        math.floor(humidity),
+        humidityDec,
         battery,
         chipId,
-        brightness
+        lux,
+        ds18b20Temp
     );
+    print(jsonBody);
     
     http.post( SERVER_ENDPOINT, 
         "Content-Type: application/x-www-form-urlencoded\r\n",
@@ -18,37 +28,83 @@ function readTempAndHumid(chipId, battery, brightness)
             print(data);
 
             print("Measurement successfull ... going to sleep");
-            node.dsleep(SLEEP_TIME);
+            --node.dsleep(SLEEP_TIME);
         end
     )
 end
 
--- Get chipId.
-local chipId = node.chipid();
+-- Get Data from DHT
+function readDHT(callback, callback2, callback3, callback4)
+    
+    if DHT_ACTIVATED == 1 then
+        status, dhtTemp, humidity, dhtTempDec, humidityDec = dht.read(PIN_DHT);
+    end
+    if DHT_ACTIVATED == 0 then
+        dhtTemp = -999;
+        dhtTempDec = 0;
+        humidity = -999;
+        humidityDec = 0;
+    end
+
+    callback(callback2, callback3, callback4);
+end
 
 -- Read battery life
-local battery;
-if BATTERY_ACTIVATED == 1 then
-    battery = adc.read(PIN_BATTERY) * 4 / 978;
-end
-if BATTERY_ACTIVATED == 0 then
-    battery = 65535;
+function readBattery(callback, callback2, callback3)
+    if BATTERY_ACTIVATED == 1 then
+        battery = adc.read(PIN_BATTERY) * 4 / 978;
+    end
+    if BATTERY_ACTIVATED == 0 then
+        battery = 65535;
+    end
+
+    callback(callback2, callback3);
 end
 
 -- Read brightness
-if BRIGHTNESS_ACTIVATED == 1 then
-    local brightnessStatus = tsl2561.init(2, 1, tsl2561.ADDRESS_FLOAT, tsl2561.PACKAGE_T_FN_CL);
-    if status == tsl2561.TSL2561_OK then
-        status = tsl2561.settiming(tsl2561.INTEGRATIONTIME_402MS, tsl2561.GAIN_1X);
+function readBrightness(callback, callback2)
+    local brightnessStatus;
+    
+    if BRIGHTNESS_ACTIVATED == 1 then
+        brightnessStatus = tsl2561.init(2, 1, tsl2561.ADDRESS_FLOAT, tsl2561.PACKAGE_T_FN_CL);
+        if brightnessStatus == tsl2561.TSL2561_OK then
+            brightnessStatus = tsl2561.settiming(tsl2561.INTEGRATIONTIME_402MS, tsl2561.GAIN_1X);
+        end
+        if brightnessStatus == tsl2561.TSL2561_OK then
+            lux = tsl2561.getlux();
+        end
     end
-    if brightnessStatus == tsl2561.TSL2561_OK then
-        local lux = tsl2561.getlux();
-        readTempAndHumid(chipId, battery, lux);
+    
+    if BRIGHTNESS_ACTIVATED == 0 then
+        lux = 384;
     end
+    
+    callback(callback2);
 end
-if BRIGHTNESS_ACTIVATED == 0 then
-    local lux = 384;
-    readTempAndHumid(chipId, battery, lux);
+
+function readDS18B20(callback)
+    if DS18B20_ACTIVATED == 1 then
+        ds18b20.setup(PIN_DS18B20)
+    
+        -- read all sensors and print all measurement results
+        ds18b20.read(
+            function(ind,rom,res,temp,tdec,par)
+                ds18b20Temp = temp;
+                
+                callback();
+            end,{}
+        );
+    end
+
+    if DS18B20_ACTIVATED == 0 then
+    
+    end
 end
 
 
+readDHT(
+    readBattery,
+    readBrightness,
+    readDS18B20,
+    sendData
+);
