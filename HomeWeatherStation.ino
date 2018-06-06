c8cdae7d5b57e640e67dd2404d3566adc91887de
Johannes Giere <johannes@jgiere.de>
2018-06-06 22:21:07 +0200

    Code aufgeräumt

--- HomeWeatherStation.ino
+++ HomeWeatherStation.ino
@@ -1,3 +1,6 @@
+
+// Copyright (C) 2018  Johannes Giere
+
 // https://github.com/adafruit/Adafruit_Sensor
 #include <Adafruit_Sensor.h>
 
@@ -22,9 +25,22 @@ void setup()
   delay(10);
 
   // We start by connecting to a WiFi network
+  connectToWiFi();
+  
+  measure();
 
-  Serial.println();
-  Serial.println();
+  enterDeepSleep();
+}
+
+void enterDeepSleep() {
+    // Disconnect from WiFi before going into deep sleep.
+  WiFi.disconnect();
+  
+  Serial.println("Going into deep sleep for 5 seconds");
+  ESP.deepSleep(5e6); // 5e6 is 5 microseconds
+}
+
+void connectToWiFi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   
@@ -43,42 +59,57 @@ void setup()
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  
+}
+
+void measure() {
   dht.setup(DHTPIN, DHTTYPE);
   Serial.println("DHT initiated");
-}
- 
-void loop() 
-{
+
   delay(dht.getMinimumSamplingPeriod());
   
-  float humidity = dht.getHumidity(); //Luftfeuchte auslesen
-  float temperature = dht.getTemperature(); //Temperatur auslesen
-  
-  // Prüfen ob eine gültige Zahl zurückgegeben wird. Wenn NaN (not a number) zurückgegeben wird, dann Fehler ausgeben.
+  float humidity = measureHumidity();
+  float temperature = measureTemperature();
+
+  // Check if valid values have been returned. If any of the values is NaN, a error is printed.
   if (isnan(temperature) || isnan(humidity)) 
   {
-    Serial.println("DHT22 konnte nicht ausgelesen werden");
-  } 
-  else
-  {
-    Serial.print(dht.getStatusString());
-    Serial.print("\t");
-    Serial.print(humidity, 1);
-    Serial.print("\t\t");
-    Serial.print(temperature, 1);
-    Serial.print("\t\t");
-    Serial.print(dht.toFahrenheit(temperature), 1);
-    Serial.print("\t\t");
-    Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
-    Serial.print("\t");
-    Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
-    Serial.print("\t");
-    Serial.println(dht.computePerception(temperature, humidity, false));
-  }
-  
-  yield();
+    Serial.println("DHT22 could not be read.");
 
-  Serial.println("Going into deep sleep for 5 seconds");
-  ESP.deepSleep(5e6); // 5e6 is 5 microseconds
+    if(isnan(temperature)) {
+      Serial.println("The temperature is not a number");
+    }
+    if(isnan(humidity)) {
+      Serial.println("The humidity is not a number");      
+    }
+    
+    return;
+  } 
+
+  
+  Serial.print(dht.getStatusString());
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.print(dht.toFahrenheit(temperature), 1);
+  Serial.print("\t\t");
+  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
+  Serial.print("\t");
+  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
+  Serial.print("\t");
+  Serial.println(dht.computePerception(temperature, humidity, false));
+    
+  yield();
 }
+
+float measureHumidity() {
+  return dht.getHumidity(); //Luftfeuchte auslesen;
+}
+
+float measureTemperature() {
+  return dht.getTemperature(); //Temperatur auslesen
+}
+
+void loop() 
+{}
